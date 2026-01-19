f24f6af4f63c35eaf13125ba42c8c324354fdd9b
KapishDev <patelkapish@gmail.com>
2026-01-20 10:39:10 +1100

    Added searching feature for jobs - user can search by position, company, location, status and job type

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -17,12 +17,21 @@ interface Job {
 export default function Jobs() {
   const [jobs, setJobs] = useState<Job[]>([])
   const [selectedJob, setSelectedJob] = useState<Job | null>(null)
+  const [allJobs, setAllJobs] = useState<Job[]>([])
   const [formData, setFormData] = useState({
     position: '',
     company: '',
     location: '',
     status: 'Applied',
     jobType: 'Full-time'
+  })
+
+  const [searchData, setSearchData] = useState({
+    position: '',
+    company: '',
+    location: '',
+    status: '',
+    jobType: ''
   })
 
   useEffect(() => {
@@ -36,6 +45,7 @@ export default function Jobs() {
       if (!res.ok) return
       const data = await res.json();
       setJobs(data);
+      setAllJobs(data);
     }
     fetchJobs();
   }, [])
@@ -55,6 +65,10 @@ export default function Jobs() {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setSearchData({ ...searchData, [e.target.name]: e.target.value })
+  }
+
   const saveJob = async () => {
     if (!selectedJob) return;
 
@@ -67,6 +81,7 @@ export default function Jobs() {
     })
     if (res.ok) {
       setJobs(jobs.map(job => job.id === selectedJob.id ? { ...job, ...formData } : job));
+      setAllJobs(allJobs.map(job => job.id === selectedJob.id ? { ...job, ...formData } : job));
       setSelectedJob(null);
     }
   }
@@ -83,12 +98,76 @@ export default function Jobs() {
 
     if (res.ok) {
       setJobs(jobs.filter(job => job.id !== selectedJob.id));
+      setAllJobs(allJobs.filter(job => job.id != selectedJob.id));
       setSelectedJob(null);
     }
   }
 
+  const filterJob = () => {
+    const filtered = allJobs.filter(job => {
+      return (
+        job.position.toLowerCase().includes(searchData.position.toLowerCase()) &&
+        job.company.toLowerCase().includes(searchData.company.toLowerCase()) &&
+        job.location.toLowerCase().includes(searchData.location.toLowerCase()) &&
+        (searchData.status && searchData.status !== "Any" ? job.status === searchData.status : true) &&
+        (searchData.jobType && searchData.jobType !== "Any" ? job.jobType === searchData.jobType : true)
+      )
+    })
+
+    console.log(searchData)
+    console.log(filtered);
+    setJobs(filtered);
+  }
+
+  const resetJobs = () => {
+    setJobs(allJobs);
+  }
+
   return (
     <div className='h-[80vh] overflow-y-auto space-y-4 p-4'>
+      <div className='h-[25vh] flex flex-start flex-col gap-2'>
+        <h1 className='font-xxl'><strong>Filter jobs</strong></h1>
+        <div className="flex flex-row gap-4 p-4 bg-muted/40 rounded-xl border shadow-sm backdrop-blur-sm">
+          <input
+            className="flex-1 px-4 py-2 rounded-lg border bg-background text-sm outline-none focus:ring-2 focus:ring-primary transition"
+            name="position"
+            onChange={handleSearchChange}
+            placeholder="🔍 Search position"
+          />
+          <input
+            className="flex-1 px-4 py-2 rounded-lg border bg-background text-sm outline-none focus:ring-2 focus:ring-primary transition"
+            name="company"
+            onChange={handleSearchChange}
+            placeholder="🏢 Search company"
+          />
+          <input
+            className="flex-1 px-4 py-2 rounded-lg border bg-background text-sm outline-none focus:ring-2 focus:ring-primary transition"
+            name="location"
+            onChange={handleSearchChange}
+            placeholder="📍 Search location"
+          />
+        </div>
+
+        <div className='flex flex-row justify-between border'>
+          <select className='flex-1 px-4 py-2 rounded-lg border bg-background text-sm outline-none focus:ring-2 focus:ring-primary transition' name="status" onChange={handleSearchChange}>
+            <option value="">Any</option>
+            <option value="Applied">Applied</option>
+            <option value="Interviewing">Interviewing</option>
+            <option value="Offer">Offer</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+          <select className="flex-1 px-4 py-2 rounded-lg border bg-background text-sm outline-none focus:ring-2 focus:ring-primary transition" name='jobType' onChange={handleSearchChange}>
+            <option value="">Any</option>
+            <option value="Full-time">Full-time</option>
+            <option value="Part-time">Part-time</option>
+            <option value="Contract">Contract</option>
+            <option value="Internship">Internship</option>
+          </select>
+        </div>
+
+        <Button onClick={filterJob}>Search Listings</Button>
+        <Button onClick={resetJobs}>Reset</Button>
+      </div>
       {jobs.length === 0 && <p>No jobs found. Start by adding a new job!</p>}
       {jobs.map((job) => ( 
         <div key={job.id} className='p-4 border rounded-lg bg-background flex justify-between items-center hover:bg-gray-100' onClick={() => openOverlay(job)}>
@@ -124,7 +203,7 @@ export default function Jobs() {
               <option>Internship</option>
             </select>
               
-            <Button className="mt-4 w-full bg-blue-500 text-white rounded hover:bg-blue-700" onClick={saveJob}>Save Changed</Button>
+            <Button className="mt-4 w-full bg-blue-500 text-white rounded hover:bg-blue-700" onClick={saveJob}>Save Changes</Button>
             <Button className="mt-2 w-full bg-red-500 text-white rounded hover:bg-red-700" onClick={removeJob}>Delete Job</Button>
           </div>
         </div>
